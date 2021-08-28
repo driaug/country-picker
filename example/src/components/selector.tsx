a31e4e5fcac54ba04f6912cc4f3596d4972ca8ba
driaug <augustyns.dries@gmail.com>
2021-08-28 19:20:25 +0200

    Added rotating icon

--- example/src/components/selector.tsx
+++ example/src/components/selector.tsx
@@ -51,7 +51,8 @@ export const CountrySelector = React.forwardRef<
             {props.selectedValue.title}
           </span>
           <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-            <svg
+            <motion.svg
+              animate={props.open ? {rotate: '90deg'} : {rotate: '0deg'}}
               className="h-5 w-5 text-gray-400"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
@@ -63,7 +64,7 @@ export const CountrySelector = React.forwardRef<
                 d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
                 clipRule="evenodd"
               />
-            </svg>
+            </motion.svg>
           </span>
         </button>
 
